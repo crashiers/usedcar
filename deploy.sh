72f620a814196bc11b13d6126cba2d0aa6e7cbae
chenyi363 <chenyi9@jd.com>
2019-01-02 15:51:56 +0800

    step2

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,4 @@ pkill java
 mvn clean package -P pro -Dmaven.test.skip=true
 pkill java
 nohup java -jar /export/web/usedcar/target/usedcar-1.0.0.jar --spring.profiles.active=pro --db.dbname=db_usedcar > /export/web/run.log 2>&1 &
+tailf /export/web/run.log
