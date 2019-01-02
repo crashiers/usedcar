b7ddcdc141918f0878cf28c18e4728415f35de6a
chenyi363 <chenyi9@jd.com>
2019-01-02 15:48:03 +0800

    step2

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 source /etc/profile
 cd /export/web/usedcar
 git pull
+pkill java
 mvn clean package -P pro -Dmaven.test.skip=true
 pkill java
 nohup java -jar /export/web/usedcar/target/usedcar-1.0.0.jar --spring.profiles.active=pro --db.dbname=db_usedcar > /export/web/run.log 2>&1 &
